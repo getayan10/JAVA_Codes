20a25bee7266364dc53c486183c5b383df5b6f42
getayan10 <112760378+getayan10@users.noreply.github.com>
2023-07-20 00:00:11 +0530

    Add files via upload

---
+++ Array/max_sum_subarray.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.ArrayList;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    arr[i]=sc.nextInt();
+		}
+		int res=arr[0];
+		int maxEnding=arr[0];
+		for(int i=1;i<n;i++)
+		{
+		    maxEnding=Math.max(maxEnding+arr[i],arr[i]);
+		    res=Math.max(res,maxEnding);
+		}
+		System.out.println(res);
+	}
+}
